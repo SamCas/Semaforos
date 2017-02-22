10e9878932fac87ec57f5d4c28f8d3b709522afa
SamDino <samuel.cas.96@gmail.com>
2017-02-21 19:48:30 -0600

    adding more methods.

--- semaforos.py
+++ semaforos.py
@@ -1,14 +1,20 @@
-#from queue import *
+from Queue import *
 from random import *
 
 ciclo_total = 120
 semaforo = {1:"Norte",2:"Sur",3:"Este",4:"Oeste"}
 
-def car_fill():
-    North = randint(2,5)
-    South = randint(2,5)
-    East = randint(2,5)
-    West = randint(2,5)
+class LaneQueue(object):
+
+    def __init__(self):
+        total_queue = 0
+
+    def lane_randomfill(self):
+        lane_queue = Queue(maxsize=0)
+        for i in range(5):
+            i = randint(2,5)
+            lane_queue.put(i)
+            print(lane_queue.get())
 
 class TrafficLight(object):
 
@@ -26,8 +32,11 @@ class TrafficLight(object):
                 print[state_change['red'], count_down]
 
 def main():
-    car_fill()
     semaforo_prueba = TrafficLight()
-    semaforo_prueba.light_change()
+    lane_prueba = LaneQueue()
+
+    # print(lane_prueba.lane_fill())
+    print(lane_prueba.lane_randomfill())
+    #semaforo_prueba.light_change()
 
 main()
